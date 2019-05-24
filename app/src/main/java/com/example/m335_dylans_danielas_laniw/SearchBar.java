a58eb8ff8068bdd1e97e99f26881bafbfb818075
JuniorBelinda <belinda@schuehle.ch>
2019-05-24 08:48:43 +0200

    searchbar refactor

--- app/src/main/java/com/example/m335_dylans_danielas_laniw/SearchBar.java
+++ app/src/main/java/com/example/m335_dylans_danielas_laniw/SearchBar.java
@@ -19,19 +19,9 @@ import java.util.List;
  */
 @SuppressLint("Registered")
 public class SearchBar extends AppCompatActivity {
+    List<Comic> comicsForSearch = new ArrayList<>();
 
-    /**
-     * Performs the search with the given search value.
-     * @param list - list of comics
-     * @param searchTextField - search field
-     * @param invalidText - invalid text textview incase there's an issue with the search
-     * @return - found comics
-     */
-    public List<Comic> performSearch(List<Comic> list, EditText searchTextField, TextView invalidText) {
-        invalidText.setVisibility(View.INVISIBLE);
-        List<Comic> comicsForSearch = new ArrayList<>();
-        //gets the user input
-        String searchedValue = searchTextField.getText().toString();
+    private List<Comic> checkSearch(String searchedValue, List<Comic> list, TextView invalidText){
 
         if (searchedValue.matches("\\*?")) {
             // Gets all of the comics if the search term is either "" or "*".
@@ -65,6 +55,24 @@ public class SearchBar extends AppCompatActivity {
             invalidText.setText(R.string.no_entries_search);
             invalidText.setVisibility(View.VISIBLE);
         }
+
+        return comicsForSearch;
+    }
+
+    /**
+     * Performs the search with the given search value.
+     * @param list - list of comics
+     * @param searchTextField - search field
+     * @param invalidText - invalid text textview incase there's an issue with the search
+     * @return - found comics
+     */
+    public List<Comic> performSearch(List<Comic> list, EditText searchTextField, TextView invalidText) {
+        invalidText.setVisibility(View.INVISIBLE);
+
+        //gets the user input
+        String searchedValue = searchTextField.getText().toString();
+
+        comicsForSearch = checkSearch(searchedValue, list, invalidText);
 
         //returns list with all comics found with the search
         return comicsForSearch;
